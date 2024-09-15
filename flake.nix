42972b97994bc9e74b3f078e1e5ae430f39fd591
Cooper Dumars <cooper@dumars.org>
2024-09-14 17:17:47 -0700

    add nix shell support

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            python3Packages.pandas
+            python3Packages.pyserial
+            python3Packages.numpy
+            python3Packages.matplotlib
+          ];
+        };
+      }
+    );
+}
